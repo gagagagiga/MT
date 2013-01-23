82452d46cdc1c14662b4a7912d99a89b2a7b9877
Chester <no_body_here@mail.com>
2013-01-24 00:24:11 +0800

    MFC, wxWidgets Multi-Threading Samples
    wxWidgets (Code::Blocks)

---
+++ wxMultiThreading/MultiThreadingApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      MultiThreadingApp.h
+ * Purpose:   Defines Application Class
+ * Author:    Chester ()
+ * Created:   2012-08-22
+ * Copyright: Chester ()
+ * License:
+ **************************************************************/
+
+#ifndef MULTITHREADINGAPP_H
+#define MULTITHREADINGAPP_H
+
+#include <wx/app.h>
+
+class MultiThreadingApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // MULTITHREADINGAPP_H
